3cab1fb8b058b2ce1823755c47a6fb0e5c1461db
sawsoemoenyunt <sawsoemoenyunt@gmail.com>
2016-04-18 21:55:47 +0630

    part 9 completed

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var displayLabel: UILabel!
     var videos = [Videos]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
         
         //Call API
         let api = APIManager()
@@ -38,6 +43,25 @@ class ViewController: UIViewController {
 //            let video = videos[i]
 //            print("\(i) name = \(video.vName)")
 //        }
+    }
+    
+    func reachabilityStatusChanged(){
+        
+        switch reachabilitystatus{
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+        displayLabel.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+        displayLabel.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+        displayLabel.text = "Reachable with Cellular"
+        default : return
+        }
+    }
+    
+    // Is called just as the object is about to be deallocated 
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
     }
     
 
